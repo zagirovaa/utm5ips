b886dec811ae0a70df3a00d80edab7f9e67aa69f
Abdul Zagirov <zagirovaa@netcon.pro>
2022-02-28 11:18:45 +0300

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name = "utm5-free-ips",
+    version = "0.1",
+    description = "Search for free ip addresses in NetUp UTM5 billing system.",
+    author = "Abdul Zagirov",
+    author_email = "zagirovaa@netcon.pro"
+)
